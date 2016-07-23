7dad763db8862dcf2c77ad13c53fe747886d53e4
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-07-23 13:22:47 -0700

    make it better

--- gulpfile.js
+++ gulpfile.js
@@ -16,23 +16,3 @@ gulp.task('watch', function(){
   // Other watchers
 });
 
-
-gulp.task("webpack-dev-server", function(callback) {
-	// modify some webpack config options
-	var myConfig = Object.create(webpackConfig);
-	myConfig.devtool = "eval";
-	myConfig.debug = true;
-
-	// Start a webpack-dev-server
-	new WebpackDevServer(webpack(myConfig), {
-		publicPath: "/" + myConfig.output.publicPath,
-		stats: {
-			colors: true
-		}
-	}).listen(8080, "localhost", function(err) {
-		if(err) throw new gutil.PluginError("webpack-dev-server", err);
-		gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
-	});
-
-	gulp.watch('style/sass/**/*.scss', ['sass']); 
-});
